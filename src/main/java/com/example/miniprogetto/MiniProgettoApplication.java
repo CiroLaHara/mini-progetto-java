80cfdcd0ef8cce68febeac421c0c9d444bc1eb83
Ciro La Hara <ciro.lahara@r2u.it>
2024-03-26 17:34:05 +0100

    Aggiorna e cancella import

--- src/main/java/com/example/miniprogetto/MiniProgettoApplication.java
+++ src/main/java/com/example/miniprogetto/MiniProgettoApplication.java
@@ -1,13 +1,7 @@
 package com.example.miniprogetto;
 
-import com.example.miniprogetto.user.User;
-import com.example.miniprogetto.user.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 
 @SpringBootApplication
